e6cb6f12a34047b15024e5569b3829d2feaa5022
Reeta Joshi <69750250+ritu-coding-way@users.noreply.github.com>
2022-04-06 18:36:24 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-/*int main(int argc, char** argv) {
-	return 0;
-}*/
 using namespace std;
-string getDayOfWeek(int dayNum){
-	string dayName;
-	Switch(dayNum){
-		
+/*int getMax(int num1,int num2){
+	int result;
+	if (num1 > num2){
+		result = num1;
+	}else{
+			result = num2;
 	}
+	return result;
+}*/
+int main(){
+	int num1, num2, result;
+	char op;
+	cout<< "Enter first  number:";
+	cin>> num1;
+	cout<< "Enter operator:";
+	cin>> op;
+	cout<< "Enter second  number:";
+	cin>> num2;
+	if(op == '+'){
+		result = num1 + num2;
+	}else if(op == '-'){
+		result = num1 - num2;
+	}else if(op == '/'){
+		result = num1 / num2;
+	}else if(op == '*'){
+		result = num1 * num2;
+	}else{
+		cout<<"Invalid operation";
+	}
+	cout<<result;
+	return 0;
+}
