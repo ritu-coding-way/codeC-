9a99d052f3fb790c938a2d2c99d0066abeb96624
Reeta Joshi <69750250+ritu-coding-way@users.noreply.github.com>
2022-04-06 18:29:37 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(int argc, char** argv) {
+	return 0;
+}*/
+using namespace std;
+string getDayOfWeek(int dayNum){
+	string dayName;
+	Switch(dayNum){
+		
+	}
---
+++ makeShape.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	cout<<"        /|-c "<<endl;
+	cout<<"       /c | -f"<<endl;
+	cout<<"___ _ / f  |_ ___-b"<<endl;
+	cout<<"\@ c  f b f c @ /-c"<<endl;
+	cout<<" @*************/f"<<endl;
+	cout<<"  @ hello     /-b"<<endl;
+	cout<<"  /    world  @-c "<<endl;
+	cout<<" /*************@ -f"<<endl;
+	cout<<"/_@_c f b f c @_@-b "<<endl;
+	cout<<"      \ f  /-c"<<endl;
+	cout<<"       \c /-f"<<endl;
+	cout<<"        \/-b"<<endl;
+	return 0;
+}
